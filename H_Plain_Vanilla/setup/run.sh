8a406a96559a546dbdbf2451f668d44d1002f5de
MendezV <som.felipe66@gmail.com>
2021-05-22 15:38:04 -0400

    data for ladder and square lattice Hubbard model with and without anisotropic hopping

--- H_Plain_Vanilla/setup/run.sh
+++ H_Plain_Vanilla/setup/run.sh
@@ -1,38 +1,72 @@
 #!/bin/bash
-readarray -t transverse_hoppings < Hops.dat
-jobname="ty_Lx_14_Ly_2"
-mkdir "../temp/temp_${jobname}"
-date "+%Y-%m-%d-%H-%M-%S" >inforun
-echo '....Jobs started running' >>  inforun 
+###########################
+#
+#Scritp that reads parameter file and
+#runs a QMC simulation for each parameter
+#in the file
+#
+#
+###########################
 
+#Readibg parameter file
+readarray -t transverse_hoppings < Hops.dat
+jobname="Lx_14_Ly_2_ty"  #JOBNAME importan to declare -has to be descriptive
+
+#General info about the job
+date_in="`date "+%Y-%m-%d-%H-%M-%S"`"
+echo "${date_in}" >inforun
+echo '....Jobs started running' >>  inforun
+
+#Temporary directories where the runs will take place
+dire_to_temps="../temp/temp_${jobname}_${date_in}"
+rm -rf "${dire_to_temps}"
+mkdir "${dire_to_temps}"
+
+#loop over the parameters
 for ty in ${transverse_hoppings[@]}; do
-	dire="../temp/temp_${jobname}/${jobname}_${ty}"
+
+	#create one temporary directory per parameter
+	dire=""${dire_to_temps}"/${jobname}_${ty}"
 	rm -rf "${dire}"
 	mkdir -vp "${dire}"
+
+	#modifying "parameter" file for the specific run
+	#and moving everything to the temp directory
 	sed "s:ham_Ty\    = 2.d0:ham_Ty\    = ${ty}.d0:g" parameters > "${dire}"/parameters
 	cp seeds "${dire}"/seeds
-	
+
+	#entering the temp directory, running and coming back
 	cd "${dire}"
 	/home/juan/Programs/ALF/Prog/ALF.out &
-	cd ..
+	cd "../../../setup"
 	sleep 1
+
 done
 
 wait
 
+#Loop over parameters to carry out statistical analysis
 for ty in ${transverse_hoppings[@]}; do
-	dire="../temp/temp_${jobname}/${jobname}_${ty}"
+
+	dire=""${dire_to_temps}"/${jobname}_${ty}"
 	cd "${dire}"
 	/home/juan/Programs/ALF/Scripts_and_Parameters_files/Start/analysis.sh &
-	cd ..
-	sleep 1 
+	cd "../../../setup"
+	sleep 1
+
 done
 
 
 wait
-dateman="-`date "+%Y-%m-%d-%H-%M-%S"`"
-echo "${dateman}" >>inforun
-echo 'Jobs finished running'>>inforun
-mkdir "../data/${jobname}_${dateman}"
-mv "${jobname}"* "../data/${jobname}_${dateman}"
 
+#general info about the job as it ends
+date_fin="`date "+%Y-%m-%d-%H-%M-%S"`"
+echo "${date_fin}" >>inforun
+echo 'Jobs finished running'>>inforun
+
+#moving files to the data directory and tidying up
+dire_to_data="../data/${jobname}_${date_fin}"
+mkdir "${dire_to_data}"
+mv "${dire_to_temps}"/* "${dire_to_data}"
+mv inforun "${dire_to_data}"
+rm -r "${dire_to_temps}"
